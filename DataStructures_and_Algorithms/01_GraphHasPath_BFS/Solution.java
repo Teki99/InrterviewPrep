3a4836fd96e4af48c03b4f4300bf0c7f626c73ff
Filip Jankovic <fjankovic068@gmail.com>
2026-06-03 23:03:32 +0200

    01_GraphHasPath_BFS Solved

--- DataStructures_and_Algorithms/01_GraphHasPath_BFS/Solution.java
+++ DataStructures_and_Algorithms/01_GraphHasPath_BFS/Solution.java
@@ -10,6 +10,42 @@ public class Solution {
      */
     public boolean hasPath(int[][] edges, int src, int dst) {
         // Your BFS solution goes here
+        // Step 1: Build the graph as an adjacency list
+        Map<Integer, List<Integer>> graph =  new HashMap<>();
+ 
+        for(int[] edge : edges)
+        {
+            // List<Integer> neighbors = graph.get(egde[0]);
+            // if(neighbors == null)
+            // {
+            //     neighbors = new ArrayList<>();
+            //     graph.put(egde[0],neighbors)
+            // }
+            // neighbors.add(edge[1]);
+            graph.computeIfAbsent(edge[0], list -> new ArrayList<>()).add(edge[1]);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+
+        queue.add(src);
+        visited.add(src);
+
+        while(!queue.isEmpty())
+        {
+            int node = queue.poll();
+            if(node == dst) return true;
+
+            for(int neighbor :  graph.getOrDefault(node, new ArrayList<>())) //iterating over a empty array is safe in case a node has no outgoing edge!
+            {
+                if(!visited.contains(neighbor))
+                {
+                    visited.add(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+        }
+        
         return false;
     }
 }
